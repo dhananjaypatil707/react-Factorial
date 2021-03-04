0193a9708ab3f7b13204b10fe3f66c95088fca18
dhananjaypatil707 <dhananjaypatil707@gmail.com>
2021-03-04 11:17:13 +0530

    1 file modified

--- src/DemoEvent.js
+++ src/DemoEvent.js
@@ -4,9 +4,14 @@ class MyEvent extends React.Component {
   handleMyClick(e) {
     console.log("Button Click");
   }
+  MyInput(e) {
+    console.log(e.target.value);
+  }
+
   render() {
     return (
       <div>
+        <input type="text" onChange={this.MyInput} />
         <button onclick={this.handleMyClick}>Okay</button>
       </div>
     );
