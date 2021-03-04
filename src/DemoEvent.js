781ccdd1e67db1acbf5ce5dc2c45af456e3d78b7
dhananjaypatil707 <dhananjaypatil707@gmail.com>
2021-03-04 12:39:11 +0530

    1 file modified

--- src/DemoEvent.js
+++ src/DemoEvent.js
@@ -23,8 +23,6 @@ class MyEvent extends React.Component {
         <button onClick={this.myButtonClick}>Okay</button>
         <p>{this.state.val2}</p>
         <p className="result">
-          {" "}
-          //ClassName is used like class in html
           <Factorial num={this.state.val1} />
         </p>
       </div>
